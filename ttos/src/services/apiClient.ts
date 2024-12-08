728c69dc9b90789f0287104dc3b1eb1aa8a3d0a0
msuttobr <rafael_lopesferreira@hotmail.com>
2024-12-07 23:34:19 -0300

    Ajustando solicitação para receber das variáveis de ambiente

--- ttos/src/services/apiClient.ts
+++ ttos/src/services/apiClient.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const apiKey = import.meta.env.VITE_API_KEY;
+const baseApi = import.meta.env.VITE_BASE_API;
+const port = import.meta.env.VITE_PORT;
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api/videos/',
+  baseURL: `${baseApi}:${port}/api/videos/`,
   headers: {
     'api-key': apiKey,
   },
